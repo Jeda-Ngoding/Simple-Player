6ce6b034b188042de0b499e3b82cf773f8bcb34a
interads <syarifuddin@interads.co.id>
2023-05-04 14:52:40 +0700

    exoplayer config

--- app/src/main/java/com/example/simpleplayer/Constant.kt
+++ app/src/main/java/com/example/simpleplayer/Constant.kt
@@ -8,3 +8,8 @@ const val MQTT_CONNECTION_TIMEOUT = 3
 const val MQTT_CONNECTION_KEEP_ALIVE_INTERVAL = 60
 const val MQTT_CONNECTION_CLEAN_SESSION = true
 const val MQTT_CONNECTION_RECONNECT = true
+
+const val MQTT_TOPIC_ROOT = "simple_player_app"
+const val MQTT_TOPIC_PUBLISH = "$MQTT_TOPIC_ROOT/test"
+const val MQTT_TOPIC_SUBSCRIBE = "$MQTT_TOPIC_ROOT/test"
+const val MQTT_TOPIC_CONTENT = "$MQTT_TOPIC_ROOT/content"
--- app/src/main/java/com/example/simpleplayer/MainActivity.kt
+++ app/src/main/java/com/example/simpleplayer/MainActivity.kt
@@ -3,23 +3,33 @@ package com.example.simpleplayer
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
+import com.google.android.exoplayer2.util.Util
 import com.google.android.material.snackbar.Snackbar
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
 import org.eclipse.paho.client.mqttv3.MqttCallbackExtended
 import org.eclipse.paho.client.mqttv3.MqttException
 import org.eclipse.paho.client.mqttv3.MqttMessage
-import java.lang.Exception
-import java.util.Calendar
 import java.util.Timer
 import kotlin.concurrent.schedule
 
 
 class MainActivity : AppCompatActivity() {
+
+    private var mPlayer: SimpleExoPlayer? = null
+    private lateinit var playerView: PlayerView
+
+    private val videoURL =
+        "https://media.geeksforgeeks.org/wp-content/uploads/20201217163353/Screenrecorder-2020-12-17-16-32-03-350.mp4"
 
     private val mqttClient by lazy {
         MQTTClientHelper(this)
@@ -29,51 +39,89 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //get PlayerView by its id
+        playerView = findViewById(R.id.playerView)
+
         setMqttCallback()
 
-        findViewById<Button>(R.id.btnPub).setOnClickListener {
-            var topic = findViewById<EditText>(R.id.editTextPubTopic).text.toString().trim()
-            val payload = findViewById<EditText>(R.id.editTextMsgPayload).text.toString()
-            if (topic.isNotEmpty()) {
-                println(
-                    try {
-                        mqttClient.publish(topic, payload, 0)
-                        "Published to topic '$topic'"
-                    } catch (ex: MqttException) {
-                        ex.printStackTrace().toString()
-                    }
-                )
-            }
-        }
-
-        findViewById<Button>(R.id.btnSub).setOnClickListener(View.OnClickListener {
-            val topic = findViewById<EditText>(R.id.editTextSubTopic).text.toString().trim()
-            if (topic.isNotEmpty()) {
-                println(
-                    try {
-                        mqttClient.subscribe(topic)
-                        "Subscribed to topic '$topic'"
-                    } catch (ex: MqttException) {
-                        "Error subscribing to topic: $topic"
-                    }
-                )
-            }
-        })
-
         Timer("CheckMqttConnection", false).schedule(3000) {
-            println("MQTT Check Connection !!!")
             if (!mqttClient.isConnected()) {
-                println("MQTT Connection failed !!!")
+                val snackbarMsg = "Failed connect to host:\n'$MQTT_HOST'."
+                Snackbar.make(findViewById(android.R.id.content), snackbarMsg, Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
             } else {
-                println("MQTT Connected !!!")
+                try {
+                    mqttClient.subscribe(MQTT_TOPIC_SUBSCRIBE.trim())
+                    Log.d("MAIN", "Subscribed to topic '$MQTT_TOPIC_SUBSCRIBE'")
+                } catch (ex: MqttException) {
+                    Log.d(
+                        "MAIN",
+                        "Error subscribing to topic: $MQTT_TOPIC_SUBSCRIBE + : ${ex.printStackTrace()}"
+                    )
+                }
             }
         }
+
+        initPlayer()
 
     }
 
     override fun onDestroy() {
         mqttClient.destroy()
         super.onDestroy()
+    }
+
+    private fun initPlayer() {
+
+        try {
+            // Create a player instance.
+            mPlayer = SimpleExoPlayer.Builder(this).build()
+
+            // Bind the player to the view.
+            playerView.player = mPlayer
+
+            //setting exoplayer when it is ready.
+            mPlayer!!.playWhenReady = true
+
+            // Set the media source to be played.
+            mPlayer!!.setMediaSource(buildMediaSource())
+
+            // Prepare the player.
+            mPlayer!!.prepare()
+
+            mPlayer!!.play()
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT >= 24) {
+            initPlayer()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (Util.SDK_INT < 24 || mPlayer == null) {
+            initPlayer()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT < 24) {
+            releasePlayer()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT >= 24) {
+            releasePlayer()
+        }
     }
 
     private fun setMqttCallback() {
@@ -89,13 +137,6 @@ class MainActivity : AppCompatActivity() {
             @Throws(Exception::class)
             override fun messageArrived(topic: String?, message: MqttMessage?) {
                 Log.w("Debug", "Message received from host '$MQTT_HOST': $message")
-                findViewById<TextView>(R.id.textViewNumMsgs).text =
-                    ("${findViewById<TextView>(R.id.textViewNumMsgs).text.toString().toInt() + 1}")
-                val str: String =
-                    "------------" + Calendar.getInstance().time + "-------------\n$message\n${
-                        findViewById<TextView>(R.id.textViewMsgPayload).text
-                    }"
-                findViewById<TextView>(R.id.textViewMsgPayload).text = str
             }
 
             override fun deliveryComplete(token: IMqttDeliveryToken?) {
@@ -104,11 +145,30 @@ class MainActivity : AppCompatActivity() {
 
             override fun connectComplete(reconnect: Boolean, serverURI: String?) {
                 val snackbarMsg = "Connected to host:\n'$MQTT_HOST'."
-                Log.w("Debug", snackbarMsg)
                 Snackbar.make(findViewById(android.R.id.content), snackbarMsg, Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
             }
 
         })
+    }
+
+    private fun releasePlayer() {
+        if (mPlayer == null) {
+            return
+        }
+        //release player when done
+        mPlayer!!.release()
+        mPlayer = null
+    }
+
+    //creating mediaSource
+    private fun buildMediaSource(): MediaSource {
+        // Create a data source factory.
+        val dataSourceFactory: DataSource.Factory = DefaultHttpDataSource.Factory()
+
+        // Create a progressive media source pointing to a stream uri.
+
+        return ProgressiveMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(MediaItem.fromUri(videoURL))
     }
 }
